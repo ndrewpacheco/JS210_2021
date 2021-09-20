b41f1c486de10a7330b709f6ea28ecc0ceba176e
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-09-19 23:38:02 -0400

    workng on flow control exercises

--- exercises/medium_1./test.js
+++ exercises/medium_1./test.js
@@ -1,9 +1,13 @@
-(false && undefined);
-((false && undefined) || (false || undefined));
-((false || undefined) || (false && undefined));
-((false && undefined) && (false || undefined));
-((false || undefined) && (false && undefined));
-('a' || (false && undefined) || '');
-((false && undefined) || 'a' || '');
-('a' && (false || undefined) && '');
-((false || undefined) && 'a' && '');
+// Book:Flow Control Exercises
+false || (true && false); false
+true || (1 + 2); true
+(1 + 2) || true;
+true && (1 + 2);
+false && (1 + 2);
+(1 + 2) && true;
+(32 * 4) >= 129;
+false !== !true; ``
+true === 4;
+false === (847 === '847');
+false === (847 == '847');
+(!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false;
