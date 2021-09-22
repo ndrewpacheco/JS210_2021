4c1c1bd64e827af474d347efe8f7939e4c18b03f
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-09-21 23:41:59 -0400

    redo flow control exercises

--- test.js
+++ test.js
@@ -1,23 +1,35 @@
-// Book:Flow Control Exercises
+// // Book:Flow Control Exercises
 
-// false || (true && false);
-// true || (1 + 2);
-// (1 + 2) || true;
-// true && (1 + 2);
-// false && (1 + 2);
-// (1 + 2) && true;
-// (32 * 4) >= 129;
-// false !== !true;
-// true === 4;
-// false === (847 === '847');
-// false === (847 == '847');
-// (!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false;
+
+
+// false || (true && false); false
+// true || (1 + 2); true
+// (1 + 2) || true; 3
+// true && (1 + 2); 3
+// false && (1 + 2); false
+// (1 + 2) && true; true
+// (32 * 4) >= 129; false
+// false !== !true;  false
+// true === 4; false
+// false === (847 === '847'); true
+// false === (847 == '847'); false
+// (!true || (!(100 / 5) === 20) || ((328 / 4) === 82)) || false;  true
 
 function evenOrOdd(num) {
-
+  if (typeof num !== 'number') {
+    return console.log("Error: not an int");
+  }
   num % 2 === 0 ? console.log('even') : console.log('odd')
 
 }
 
 evenOrOdd(3);
-evenOrOdd(4);
+evenOrOdd('df');
+
+
+return foo() ? 'bar' : qux();
+
+if (foo()) {
+  'bar';
+}
+
