3f94edea9beb5f3b3c660614214790e384d5d5ee
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-09-26 14:41:07 -0400

    working on array exercises

--- test.js
+++ test.js
@@ -1,18 +1,13 @@
-function randomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function findIntegers(things) {
+  return things.filter(element => Number.isInteger(element));
 }
 
-// let tries = 0;
-let result = randomNumberBetween(1, 6);
-// tries += 1;
 
-// while (result <= 2) {
-//   result = randomNumberBetween(1, 6);
-//   tries += 1;
-// }
 
-let tries = 0;
-for (result; result <= 2; tries += 1) {
-   result = randomNumberBetween(1, 6);
-}
-console.log('It took ' + String(tries) + ' tries to get a number greater than 2');
+
+
+let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+let integers = findIntegers(things);
+console.log(integers); // => [1, 3, -4]
+
+
