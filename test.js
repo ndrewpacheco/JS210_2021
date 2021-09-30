bb9fe8c1872ccafbc755a8095914de6b2063e5b5
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-09-30 14:31:45 -0400

    more exercises on objects

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
-function includesThree(numbers) {
-  return numbers.includes(3);
-}
+let myProtoObj = {
+  foo: 1,
+  bar: 2,
+};
 
-
-let numbers1 = [1, 3, 5, 7, 9, 11];
-let numbers2 = [];
-let numbers3 = [2, 4, 6, 8];
-
-
-console.log(includesThree(numbers1));
-console.log(includesThree(numbers2));
-console.log(includesThree(numbers3));
+let myObj = Object.create(myProtoObj);
