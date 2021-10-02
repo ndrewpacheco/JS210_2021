e6e758936d50ff203937b058c4b424fa22277b7e
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-10-01 23:12:06 -0400

    working on more stuff chapter

--- test.js
+++ test.js
@@ -1,6 +1,30 @@
-let myProtoObj = {
+// let myProtoObj = {
+//   foo: 1,
+//   bar: 2,
+//   qux: 3,
+// };
+
+// let myObj = Object.create(myProtoObj);
+
+function copyObj(obj, arr = []) {
+  // if array is empty, return obj
+  if (arr.length === 0) return obj;
+  newObj = {}
+  arr.forEach(element => newObj[element] = obj[element]);
+  return newObj;
+}
+
+let objToCopy = {
   foo: 1,
   bar: 2,
+  qux: 3,
 };
 
-let myObj = Object.create(myProtoObj);
+let newObj = copyObj(objToCopy);
+console.log(newObj);        // => { foo: 1, bar: 2, qux: 3 }
+
+let newObj2 = copyObj(objToCopy, [ 'foo', 'qux' ]);
+console.log(newObj2);       // => { foo: 1, qux: 3 }
+
+let newObj3 = copyObj(objToCopy, [ 'bar' ]);
+console.log(newObj3);       // => { bar: 2 }
