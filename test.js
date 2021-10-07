ec25f59e2856e4042f174605d73cc060016f0127
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-10-07 14:20:42 -0400

    working on scope exercises

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-// We need a piece of reusable code that returns the average of three numbers.
- // Define a function named average that takes three parameters, a, b, and c,
-  // and returns their average.
-   // Call the function with three numbers and log the result.
+let a = 'outer';
 
-
-function average(a,b,c) {
-  return (a + b + c) / 3;
+function testScope() {
+  a = 'inner';
+  console.log(a);
 }
 
-console.log(average(1,2,3));
+console.log(a);
+testScope();
+console.log(a);
