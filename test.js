0abcb22a5355482c458c37051c4443465662b762
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-10-21 12:20:10 -0400

    working on flow cotrol exercises

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
+function gcd(num1, num2) {
+  let denominator;
+  let largestNum  = num1 > num2 ? num1 : num2;
 
-let a = 'outer';
+  for (let counter = 1; counter <= largestNum; counter += 1) {
+    if (num1 % counter == 0 && num2 % counter == 0) denominator = counter;
+  }
 
-console.log(a);
-setScope();
-console.log(a);
+  return denominator;
+}
 
-var setScope = function () {
-  a = 'inner';
-};
-
+console.log(gcd(12, 4));  // 4
+console.log(gcd(15, 10));  // 5
+console.log(gcd(9, 2));  // 1
