6efa3a62a3f41c3f8679b73b669c18a132c69bdb
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-10-22 11:07:46 -0400

    working on trim ex

--- test.js
+++ test.js
@@ -1,31 +1,6 @@
-function indexOf(firstString, secondString) {
-  for (let index = 0; index < firstString.length; index += 1) {
-    let matchingString = '';
-    for (let matchingIndex = index; matchingIndex < index + secondString.length; matchingIndex += 1) {
-      matchingString += firstString[matchingIndex];
-    }
-
-    if (matchingString === secondString) return index;
-  }
-  return -1;
-}
-
-function lastIndexOf(firstString, secondString) {
-  for (let index = firstString.length - 1; index >= 0; index -= 1) {
-    let matchingString = '';
-    for (let matchingIndex = index; matchingIndex < index + secondString.length; matchingIndex += 1) {
-      matchingString += firstString[matchingIndex];
-    }
-    if (matchingString === secondString) return index;
-  }
-  return -1;
-}
-
-// console.log(indexOf('Some strings', 's'));                      // 5
-// console.log(indexOf('Blue Whale', 'Whale'));                    // 5
-// console.log(indexOf('Blue Whale', 'Blute'));                    // -1
-// console.log(indexOf('Blue Whale', 'leB'));                      // -1
-
-console.log(lastIndexOf('Some strings', 's'));                  // 11
-console.log(lastIndexOf('Blue Whale, Killer Whale', 'Whale'));  // 19
-console.log(lastIndexOf('Blue Whale, Killer Whale', 'all'));    // -1
+trim('  abc  ');  // "abc"
+trim('abc   ');   // "abc"
+trim(' ab c');    // "ab c"
+trim(' a b  c');  // "a b  c"
+trim('      ');   // ""
+trim('');         // ""
