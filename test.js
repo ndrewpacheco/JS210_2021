6718d0cd358f7b18d2a95cb3dc757cde1fadc2f6
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-10-25 13:11:36 -0400

    rot13 workings

--- test.js
+++ test.js
@@ -1,6 +1,36 @@
-trim('  abc  ');  // "abc"
-trim('abc   ');   // "abc"
-trim(' ab c');    // "ab c"
-trim(' a b  c');  // "a b  c"
-trim('      ');   // ""
-trim('');         // ""
+function substring(string, start, end = string.length) {
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+
+  if (start < 0 || isNaN(start)) {
+    start = 0;
+  }
+
+  if (end < 0 || isNaN(end)) {
+    end = 0;
+  }
+
+  if (start === end) {
+    return '';
+  }
+
+  let result = '';
+  for (let index = start; index < end; index += 1) {
+    if (index >= string.length) break;
+    result += string[index];
+  }
+  return result;
+}
+
+let string = 'hello world';
+
+console.log(substring(string, 2, 4));    // "ll"
+console.log(substring(string, 4, 2));    // "ll"
+console.log(substring(string, 2, 2));    // ""
+console.log(substring(string, 0, -1));   // ""
+
+
+console.log(substring(string, 2));       // "llo world"
+console.log(substring(string, 'a'));     // "hello world"
+console.log(substring(string, 8, 20));   // "rld"
