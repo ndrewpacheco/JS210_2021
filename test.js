2a876ea1eee2336696c5dd966303ffaa20cc757e
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-10-27 12:08:28 -0400

    working with array methods

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-let shift = (arr, value) => {
-  let shiftElement = arr[0];
-  for (index = 0; index < arr.length; index += 1) {
-    arr[index] = arr[index + 1];
-  }
-  arr.length -= 1;
-  return shiftElement;
+function join(arr, str) {
+  let resultString = '';
 
+  for (let index = 0; index < arr.length; index += 1) {
+    resultString += arr[index];
+    if (index !== arr.length - 1) {
+      resultString += str;
+    }
+  }
+
+  return resultString;
 }
 
-
-let count = [1, 2, 3];
-shift(count);           // 1
-count;                  // [ 2, 3 ]
+join(['bri', 'tru', 'wha'], 'ck ');       // 'brick truck wha'
+join([1, 2, 3], ' and ');                 // '1 and 2 and 3'
