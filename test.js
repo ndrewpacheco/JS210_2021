2a6cdc4de507240bc3dd25efd3f8be92d58ab1e1
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-11-02 16:34:45 -0400

    workingo n penultimate ex

--- test.js
+++ test.js
@@ -1,24 +1,6 @@
-function repeatedCharacters(str) {
-  let allChars = {};
-  str.split('').forEach(function(char) {
-    char = char.toLowerCase();
-    if (allChars[char]) {
-      allChars[char] += 1;
-    } else {
-      allChars[char] = 1;
-    }
-  });
-
-  let filterChars = Object.keys(allChars).filter(char => allChars[char] > 1);
-  let result = {};
-
-  filterChars.forEach(function(char){
-    result[char] = allChars[char];
-  });
-  console.log(result);
+function penultimate(string) {
+  return string.split(' ')[-2];
 }
-repeatedCharacters('Programming');    // { r: 2, g: 2, m: 2 }
-repeatedCharacters('Combination');    // { o: 2, i: 2, n: 2 }
-repeatedCharacters('Pet');            // {}
-repeatedCharacters('Paper');          // { p: 2 }
-repeatedCharacters('Baseless');      // { s: 3, e: 2 }
+
+penultimate('last word');                    // expected: "last"
+penultimate('Launch School is great!');      // expected: "is"
