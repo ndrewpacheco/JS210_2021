afbd99802c8663525894e10c3b035737173e1ed2
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-11-04 13:38:07 -0400

    working on medium 1 fundamentals

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-function penultimate(string) {
-  return string.split(' ')[-2];
-}
-
-penultimate('last word');                    // expected: "last"
-penultimate('Launch School is great!');      // expected: "is"
+return "sup"
