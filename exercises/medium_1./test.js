b6c547d27b52fb81c5bb95b207df7bbce1e7529e
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-11-06 16:26:24 -0400

    onto easy 4

--- exercises/medium_1./test.js
+++ exercises/medium_1./test.js
@@ -1,19 +1,4 @@
-(false && undefined);
-// false
-(false || undefined);
-// false
-((false && undefined) || (false || undefined));
-// undefined
-((false || undefined) || (false && undefined));
-// undefined
-((false && undefined) && (false || undefined));
-// false
-((false || undefined) && (false && undefined));
-
-('a' || (false && undefined) || '');
-
-((false && undefined) || 'a' || '');
-
-('a' && (false || undefined) && '');
-
-((false || undefined) && 'a' && '');
+Welcome
+to
+the
+matrix
