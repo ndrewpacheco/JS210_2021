b774b526cb5e027163e92b7974958bbb04b25178
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-11-06 11:53:56 -0400

    onto palindrome ex

---
+++ exercises/easy_3/worksheet.js
@@ -0,0 +1,4 @@
+isPalindrome('madam');               // true
+isPalindrome('Madam');               // false (case matters)
+isPalindrome("madam i'm adam");      // false (all characters matter)
+isPalindrome('356653');              // true
