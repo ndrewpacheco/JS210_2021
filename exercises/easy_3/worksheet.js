16d4a5e342e50b37a011659d4c57ec0a1a3a5632
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-11-06 16:25:57 -0400

    onto esy4

--- exercises/easy_3/worksheet.js
+++ exercises/easy_3/worksheet.js
@@ -1,4 +1,16 @@
-isPalindrome('madam');               // true
-isPalindrome('Madam');               // false (case matters)
-isPalindrome("madam i'm adam");      // false (all characters matter)
-isPalindrome('356653');              // true
+function wordSizes(str) {
+  if (str.length === 0) return {};
+
+  let result = {};
+  let sizes = str.split(' ').map (word => word.length);
+  sizes.forEach( size => {
+    (result[size]) ? result[size] += 1 : result[size] = 1
+  })
+  return result;
+
+}
+
+wordSizes('Four score and seven.');
+wordSizes('Hey diddle diddle, the cat and the fiddle!');
+wordSizes("What's up doc?");
+wordSizes('');
