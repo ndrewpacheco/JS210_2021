4a88188c1483d1362072f141753548c562161f11
Andrew  Pacheco <ndrewpacheco@gmail.com>
2021-11-09 10:50:21 -0500

    working on easy exer

--- exercises/easy_4/worksheet.js
+++ exercises/easy_4/worksheet.js
@@ -1,1 +1,17 @@
-multiplyList([3, 5, 7], [9, 10, 11]);    // [27, 50, 77]
+function doubleConsonants(str) {
+  let result = '';
+  str.split('').forEach(char => {
+    if (isValid(char)) result += char;
+    result += char;
+  });
+  console.log(result);
+}
+function isValid(char) {
+  let consonantRegex = /[bcdfghjklmnpqrstvwxyq]/gi;
+  return char.match(consonantRegex);
+}
+
+doubleConsonants('String');          // "SSttrrinngg"
+doubleConsonants('Hello-World!');    // "HHellllo-WWorrlldd!"
+doubleConsonants('July 4th');        // "JJullyy 4tthh"
+doubleConsonants('');                // ""
